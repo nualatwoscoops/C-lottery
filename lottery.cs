f7633b3ed6fcf110c409cd64428af603070916dc
20145955 <20145955@tafe.wa.edu.au>
2025-08-28 19:36:27 +0800

    why cant i see any values

--- lottery.cs
+++ lottery.cs
@@ -1,36 +1,51 @@
-﻿
-int drawSize = 6;
-int minNumber = 1;
-int maxNumber = 10; 
+﻿using System;
 
-int[] userNumbers= new int[drawSize];
-//int[] randomNumbers= new int[drawSize];
+class Program
+{ 
+    static void Main()
+    { 
+        int drawSize = 3;
+        int minNumber = 1;
+        int maxNumber = 10;
 
-for (int i = 0; i < userNumbers.Length; i++)
-{
-    Console.WriteLine("Enter a number between " + minNumber + " and " + maxNumber + " : ");
-    string userInput = Console.ReadLine();
-    int number;
+        int[] userNumbers = new int[drawSize];
+        int count = 0;
+        //int[] randomNumbers= new int[drawSize];
 
-    if (int.TryParse(userInput, out number))
-    {
-        userNumbers[i] = number; //stores
-        Console.WriteLine("Your number is: " + number);
-    }
-    else
-    {
-        Console.WriteLine("Not a valid entry. Try again.");
-        i--;
+        Console.WriteLine("Enter " + drawSize + " numbers between " + minNumber + " and " + maxNumber + " : ");
+        string userInput = Console.ReadLine();
+
+                string[] parts = userInput.Split(' ');
+
+        for (int i = 0; i <parts.Length && count < drawSize; i++)
+        {
+            int number;
+            if (int.TryParse(parts[i], out number) && number >= minNumber && number <= maxNumber) 
+            {
+                userNumbers[count] = number;
+                count++;
+            }
+            else
+            {
+                Console.WriteLine(parts[i] + " are invalid entries. Try again.");
+            }
+        }
+
+        Console.WriteLine("Your numbers are: ");
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(userNumbers[i]);
+            if (i < count - 1 ) Console.Write(", ");
+        }
+        Console.WriteLine();
     }
 }
+//userNumbers[0] = 0;
+//userNumbers[1] = 1;
+//userNumbers[2] = 2;
+//userNumbers[3] = 3;
+//userNumbers[4] = 4;
+//userNumbers[5] = 5;
 
+//Array.Sort(userNumbers);
 
-Array.Sort(userNumbers);
-
-//replace from input later
-userNumbers[0] = 0;
-userNumbers[1] = 1;
-userNumbers[2] = 2;
-userNumbers[3] = 3;
-userNumbers[4] = 4;
-userNumbers[5] = 5;
