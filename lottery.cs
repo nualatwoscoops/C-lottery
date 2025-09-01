76c1bed0dea8f212cba4b4b81d0fb51350c63db9
20145955 <20145955@tafe.wa.edu.au>
2025-09-01 20:07:04 +0800

    Debug input validation and random numbers working

--- lottery.cs
+++ lottery.cs
@@ -1,51 +1,82 @@
 ﻿using System;
+using System.Numerics;
 
 class Program
-{ 
+{
     static void Main()
-    { 
+    {
         int drawSize = 3;
         int minNumber = 1;
         int maxNumber = 10;
 
         int[] userNumbers = new int[drawSize];
-        int count = 0;
-        //int[] randomNumbers= new int[drawSize];
 
-        Console.WriteLine("Enter " + drawSize + " numbers between " + minNumber + " and " + maxNumber + " : ");
-        string userInput = Console.ReadLine();
-
-                string[] parts = userInput.Split(' ');
-
-        for (int i = 0; i <parts.Length && count < drawSize; i++)
+        while (true)
         {
-            int number;
-            if (int.TryParse(parts[i], out number) && number >= minNumber && number <= maxNumber) 
+
+            Console.WriteLine("Enter " + drawSize + " numbers between " + minNumber + " and " + maxNumber + " : ");
+            string[] parts = Console.ReadLine().Split(' ');
+
+            if (parts.Length != drawSize)
             {
-                userNumbers[count] = number;
-                count++;
+                Console.WriteLine("You must enter exactly " + drawSize + " numbers. Try again.");
+                continue;
             }
-            else
+
+            bool allValid = true;
+            for (int i = 0; i < drawSize; i++)
             {
-                Console.WriteLine(parts[i] + " are invalid entries. Try again.");
+                if (int.TryParse(parts[i], out int number) && number >= minNumber && number <= maxNumber)
+                {
+                    userNumbers[i] = number;
+                }
+                else
+                {
+                    Console.WriteLine("Boooo. Don't you know how a lotto works? " + parts[i] + " is an invalid entry. You must try again.");
+                    allValid = false;
+                    break;
+                }
             }
+
+            if (allValid) break;
         }
 
-        Console.WriteLine("Your numbers are: ");
-        for (int i = 0; i < count; i++)
+        Console.WriteLine("Your numbers are: " + string.Join(", ", userNumbers));
+
+
+        Random rnd = new Random();
+        int[] randomNumbers = new int[drawSize];
+
+        for (int i = 0; i < drawSize; i++)
         {
-            Console.Write(userNumbers[i]);
-            if (i < count - 1 ) Console.Write(", ");
+            randomNumbers[i] = rnd.Next(minNumber, maxNumber + 1);
         }
-        Console.WriteLine();
+        Console.Write("The random numbers are: ");
+        Console.WriteLine(string.Join(", ", randomNumbers));
     }
 }
-//userNumbers[0] = 0;
-//userNumbers[1] = 1;
-//userNumbers[2] = 2;
-//userNumbers[3] = 3;
-//userNumbers[4] = 4;
-//userNumbers[5] = 5;
-
 //Array.Sort(userNumbers);
 
+
+//for (int i = 0; i < count; i++)
+//{
+//    Console.Write(userNumbers[i]);
+//    if (i < count - 1) Console.Write(", ");
+//}
+//Console.WriteLine();
+
+//Random rnd = new Random();
+//int[] randomNumbers = new int[drawSize];
+
+//for (int i = 0; i < drawSize; i++)
+//{
+//    randomNumbers[i] = rnd.Next(minNumber, maxNumber + 1);
+//}
+//Console.Write("The random numbers are: ");
+//for (int i = 0; i < drawSize; i++)
+//{
+//    Console.Write(randomNumbers[i] + " ");
+//    if (i < drawSize - 1) Console.Write(", ");
+//}
+//Console.WriteLine();
+//        }
--- obj/Debug/net8.0/Assignment 1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assignment 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59363e11ca85a6488c4d6826c1d07d4c53052a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7633b3ed6fcf110c409cd64428af603070916dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
